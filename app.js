6bdd183f80c13667602ebe8144b9ab15860ef798
Dante Fragapane <dante.fragapane@gmail.com>
2019-06-15 19:07:32 -0400

    Setting up imports, starting the basics of reading the input file

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
+// Imports
+const readline = require('readline')
 const fs = require('fs')
 
+// Variables
+const mbox = './mbox.full'
+
+// =====================
+// =====================
+
+// Read the file line by line
+let lineReader = readline.createInterface({
+  input: fs.createReadStream(mbox)
+})
+
+// Event listener for each line that was read
+lineReader.on('line', function (line) {
+  console.log(line)
+})
