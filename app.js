ecc042280dd43aeab367e069bca4a0414f7d3e39
Dante Fragapane <dante.fragapane@gmail.com>
2019-06-15 21:05:40 -0400

    Started work on the parsing logic Created a function to compile each individual email into one array

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ const readline = require('readline')
 const fs = require('fs')
 
 // Variables
-const mbox = './mbox.full'
+const mbox = './MboxFormatterChallenge/mbox.full'
+let emailBool = false // This variable will be used inside the reading logic to determine which lines are part of an email
+let email = []
+let emailList = []
 
 // =====================
 // =====================
+
+const emailCompile = function emailCompile (emailList, emaili) {
+  emailList.push(email.join('\n'))
+  return emailList
+}
 
 // Read the file line by line
 let lineReader = readline.createInterface({
@@ -15,5 +23,17 @@ let lineReader = readline.createInterface({
 
 // Event listener for each line that was read
 lineReader.on('line', function (line) {
-  console.log(line)
+  if (line.match(/^From /)) {
+    emailBool = true
+  } else if (emailBool) {
+    email.push(line)
+  }
+  if (email && line.match(/1.4.0.g6f2b/)) {
+    emailBool = false
+    emailList = emailCompile(emailList, email)
+  }
+})
+
+lineReader.on('close', function () {
+  // console.log(email)
 })
