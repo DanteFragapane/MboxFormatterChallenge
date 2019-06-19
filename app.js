b311f115d9c1439ead3aa4696c6b113ac505ed08
Dante Fragapane <dante.fragapane@gmail.com>
2019-06-19 10:29:15 -0400

    Continued work on parsing the mbox file

--- app.js
+++ app.js
@@ -3,16 +3,21 @@ const readline = require('readline')
 const fs = require('fs')
 
 // Variables
-const mbox = './MboxFormatterChallenge/mbox.full'
+const mbox = 'mbox.full'
 let emailBool = false // This variable will be used inside the reading logic to determine which lines are part of an email
 let email = []
 let emailList = []
+let header = ''
+let signature = ''
 
 // =====================
 // =====================
 
-const emailCompile = function emailCompile (emailList, emaili) {
-  emailList.push(email.join('\n'))
+const emailCompile = function emailCompile(emailList, header, emaili) {
+  emailList.push({
+    header: header,
+    message: email.join('\n')
+  })
   return emailList
 }
 
@@ -24,16 +29,19 @@ let lineReader = readline.createInterface({
 // Event listener for each line that was read
 lineReader.on('line', function (line) {
   if (line.match(/^From /)) {
+    email = []
+    header = line
     emailBool = true
   } else if (emailBool) {
     email.push(line)
   }
   if (email && line.match(/1.4.0.g6f2b/)) {
+    signature = line
     emailBool = false
-    emailList = emailCompile(emailList, email)
+    emailList = emailCompile(emailList, header, email)
   }
 })
 
 lineReader.on('close', function () {
-  // console.log(email)
+  console.log('Done!')
 })
